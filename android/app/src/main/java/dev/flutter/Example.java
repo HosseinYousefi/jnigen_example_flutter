d4b5f736abe15cd48c791bd313178d6e8791cb3f
Hossein Yousefi <yousefi@google.com>
2023-03-08 13:47:55 +0100

    initial commit

---
+++ android/app/src/main/java/dev/flutter/Example.java
@@ -0,0 +1,7 @@
+package dev.flutter;
+
+public class Example {
+  public static int sum(int a, int b) {
+    return a + b;
+  }
+}
---
+++ android/app/src/main/kotlin/com/example/jnigen_example_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.jnigen_example_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
