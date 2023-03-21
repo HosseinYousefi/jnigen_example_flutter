bb2d7488deccf191999c4c0a16dfec550fc5dca2
Hossein Yousefi <yousefi@google.com>
2023-03-21 11:03:44 +0100

    typo fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:jnigen_example_flutter/example.dart';
+
+import 'example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'JNIGen Demo',
+      title: 'JNIgen Demo',
       home: Adder(),
     );
   }
